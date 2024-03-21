48d87c2be57ba1b7f461a72ca75fc1f82be24594
похотливый тритон <nico@scheucher.wien>
2024-03-21 10:45:15 +0100

    Update powershell.py

--- Scripts/powershell.py
+++ Scripts/powershell.py
@@ -1,10 +1,32 @@
-import winrm
+import paramiko
 
-# Erstelle eine Verbindung zum Windows-Server
-session = winrm.Session('10.3.14.121', auth=('NIFEDE\Administrator', ''))
+# SSH-Verbindungsinformationen
+hostname = '10.3.14.121'
+port = 22  # Standard-SSH-Port ist 22
+username = 'Administrator'
+password = 'Start123$'
 
-# Führe das whoami-Kommando auf dem Server aus
-result = session.run_cmd('whoami')
+# Erstellen einer SSH-Verbindung
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-# Gib das Ergebnis aus
-print("whoami output:", result.std_out.decode())
+try:
+    ssh_client.connect(hostname, port, username, password)
+    print("Erfolgreich mit dem Server verbunden.")
+
+    # Ausführen von ipconfig-Befehl
+    stdin, stdout, stderr = ssh_client.exec_command('ipconfig')
+    
+    # Ausgabe lesen
+    output = stdout.read().decode()
+    
+    print("Ausgabe von 'ipconfig' auf dem Server:")
+    print(output)
+    
+except paramiko.AuthenticationException:
+    print("Authentifizierung fehlgeschlagen. Überprüfen Sie Ihre Anmeldeinformationen.")
+except paramiko.SSHException as ssh_err:
+    print(f"Fehler beim Verbindungsaufbau: {ssh_err}")
+finally:
+    # SSH-Verbindung schließen
+    ssh_client.close()
